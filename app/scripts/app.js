6b2a6744980bf1c46321ffeda1c0d7ea20ea186c
Chris Laughlin <bluntm@gmail.com>
2013-06-09 17:55:41 +0100

    Adding scaffold generated from node and yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('chitterPrototypeApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
