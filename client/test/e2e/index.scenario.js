8f66ab94600a116df58c993ba3a64d0f3e9f7ac4
Martin Micunda <martinmicunda@hotmail.com>
2013-07-27 00:07:47 +0100

    Add underscore library and e2e test

--- client/test/e2e/index.scenario.js
+++ client/test/e2e/index.scenario.js
@@ -1,10 +1,14 @@
 describe('my app', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('/');
-  });
+    beforeEach(function() {
+        browser().navigateTo('/');
+    });
 
-  it('should be publicly accessible and default route to be /projectsinfo', function() {
-    expect(browser().location().path()).toBe("/");
-  });
+    it('should be publicly accessible and default route to be /', function() {
+        expect(browser().location().path()).toBe("/");
+    });
+
+    it("should have tag h1 with text 'Welcome to Chitter!'", function() {
+        expect(element("h1").text()).toEqual("Welcome to Chitter!");
+    });
 });
